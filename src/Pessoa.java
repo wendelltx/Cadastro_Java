f1b0b03fffcff6f780afa481496faae59ac742a2
Wendell Teixeira <wendelltx.dev@gmail.com>
2026-04-22 20:04:05 -0300

    Primiera versão

---
+++ src/Pessoa.java
@@ -0,0 +1,14 @@
+
+public class Pessoa {
+	String nome;	
+	int idade;
+	String cpf;
+	
+	public Pessoa(String nome, int idade, String cpf) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.cpf = cpf;
+	}
+	
+}
