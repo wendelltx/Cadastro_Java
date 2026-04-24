7e3193c00cb89b6be2d07bf4190d4ab506223136
Wendell Teixeira <wendelltx.dev@gmail.com>
2026-04-24 20:16:36 -0300

    alteração feita em aula

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,8 +1,33 @@
 
 public class Pessoa {
+	
 	String nome;	
 	int idade;
 	String cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 	
 	public Pessoa(String nome, int idade, String cpf) {
 		super();
